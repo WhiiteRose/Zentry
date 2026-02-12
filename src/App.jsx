469be47f90367382a0483655be288da7b489b20b
WhiiteRose <tom.roger@epitech.eu>
2026-02-12 23:42:22 +0100

    [✨] Add a new Hero component with video playback and GSAP animations, integrating it into the main application.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <section className="min-h-screen z-0 bg-blue-500"></section>
     </main>
   );
 }
