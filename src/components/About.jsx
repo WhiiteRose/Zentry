6c6b5a3c4e2b8ca801821854273ed1196f9e0ab7
WhiiteRose <tom.roger@epitech.eu>
2026-02-19 18:11:53 +0100

    [🎨] add About component with GSAP scroll-triggered clip path animation and introductory content.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
-import { useGSAP } from '@gsap/react';
-import React from 'react';
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/all';
+
 import AnimatedTitle from './AnimatedTitle';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -22,7 +22,7 @@ const About = () => {
     clipAnimation.to('.mask-clip-path', {
       width: '100vw',
       height: '100vh',
-      borderRadius: '0',
+      borderRadius: 0,
     });
   });
 
@@ -32,24 +32,27 @@ const About = () => {
       className="min-h-screen w-screen"
     >
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <h2 className="font-general text-sm uppercase md:text-[10px]">
+        <p className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
-        </h2>
+        </p>
+
         <AnimatedTitle
-          title="Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared"
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
           containerClass="mt-5 !text-black text-center"
         />
 
         <div className="about-subtext">
           <p>
-            The Game of Games begins-your life, now an epic MMORPGG
+            The Game of Games begins—your life, now an epic MMORPG
           </p>
-          <p>
+          <p className="text-gray-500">
             Zentry unites every player from countless games and
-            platforms
+            platforms, both digital and physical, into a unified Play
+            Economy
           </p>
         </div>
       </div>
+
       <div
         className="h-dvh w-screen"
         id="clip"
