5912e61f1f761ea9563c0ee27d2360c81cdb5d9c
WhiiteRose <tom.roger@epitech.eu>
2026-02-19 17:42:36 +0100

    [💄] introduce About section with animated title and scroll-triggered image clip effect.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from '@gsap/react';
 import React from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import AnimatedTitle from './AnimatedTitle';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -34,10 +35,8 @@ const About = () => {
         <h2 className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
         </h2>
-        <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-          Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared
-          adventure
-        </div>
+        <AnimatedTitle />
+
         <div className="about-subtext">
           <p>
             The Game of Games begins-your life, now an epic MMORPGG
