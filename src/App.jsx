4e69ee657f0edb1d8b372d6b7b68418e960d2074
WhiiteRose <tom.roger@epitech.eu>
2026-02-20 13:59:58 +0100

    [✨] Add Contact components and integrate them into the main application layout.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import About from './components/About';
 import NavBar from './components/NavBar';
 import Features from './components/Features';
 import Story from './components/Story';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
       <About />
       <Features />
       <Story />
+      <Contact />
+      <Footer />
     </main>
   );
 }
